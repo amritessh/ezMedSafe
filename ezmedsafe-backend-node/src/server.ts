5941a92c7b0d49faf3447315044018432290ed93
Amritesh Anand <amriteshanand7@gmail.com>
2025-05-26 12:46:27 -0400

    updating server.ts and .env

---
+++ ezmedsafe-backend-node/src/server.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.resolve(__dirname, '../.env') });
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json()); 
+
+app.get('/health', (req, res) => {
+  res.status(200).send('ezMedSafe Backend OK');
+});
+
+app.listen(PORT, () => {
+  console.log('ezMedSafe Backend is running on port ${PORT}');
+});
